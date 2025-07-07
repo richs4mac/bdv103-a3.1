651fa3a59a2bed4f048e61acdd40d89cdfb4141f
richs4mac <210944006+richs4mac@users.noreply.github.com>
2025-07-07 02:22:30 +0000

    fix type issues in assignment 1

--- adapter/assignment-1.ts
+++ adapter/assignment-1.ts
@@ -6,21 +6,21 @@ export interface Book {
     image: string,
 };
 
-async function listBooks(filters?: Array<{from?: number, to?: number}>) : Promise<Book[]>{
+async function listBooks(filters?: Array<{ from?: number, to?: number; }>): Promise<Book[]> {
     // We want to generate the query string to match the format expected by qs: https://www.npmjs.com/package/qs
-    let query = filters?.map(({from, to}, index) => {
+    const query = filters?.map(({ from, to }, index) => {
         let result = "";
         if (from) {
             result += `&filters[${index}][from]=${from}`;
         }
         if (to) {
-            result += `&filters[${index}][to]=${to}`
+            result += `&filters[${index}][to]=${to}`;
         }
         return result;
     }).join("&") ?? "";
 
     // We then make the request
-    let result = await fetch(`http://localhost:3000/books?${query}`);
+    const result = await fetch(`http://localhost:3000/books?${query}`);
 
     if (result.ok) {
         // And if it is valid, we parse the JSON result and return it.
